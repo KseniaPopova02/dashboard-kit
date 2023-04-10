6fdd67bf93c1cede80388e8059c1be847dd1294c
Ksenia <kseniapopova02@gmail.com>
2023-04-10 22:50:58 +0200

    install: react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
-const App = () => <></>;
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import {
+  Forgot,
+  ForgotSuccess,
+  LogIn,
+  Reset,
+  SignUp,
+  NotFound,
+  Layout,
+} from "./modal";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Forgot />} />
+        <Route path="ForgotSuccess" element={<ForgotSuccess />} />
+        <Route path="LogIn" element={<LogIn />} />
+        <Route path="Reset" element={<Reset />} />
+        <Route path="SignUp" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
