314869df74df66cfb8cf09a6e027181d92cc6de1
Ksenia <kseniapopova02@gmail.com>
2023-04-11 17:04:49 +0200

    remove: className from InputPassword

--- src/components/InputPassword/InputPassword.jsx
+++ src/components/InputPassword/InputPassword.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import { StyledInput } from "./style";
 
 export const InputPassword = ({ type = "text", placeholder = "" }) => (
-  <StyledInput
-    className="input__password"
-    placeholder={placeholder}
-    type={type}
-  />
+  <StyledInput placeholder={placeholder} type={type} />
 );
--- src/components/InputPassword/style.js
+++ src/components/InputPassword/style.js
@@ -2,20 +2,18 @@ import styled from "styled-components";
 import { Input } from "antd";
 
 export const StyledInput = styled(Input.Password)`
-  &.input__password {
-    &.ant-input-affix-wrapper {
-      border: 1px solid #dfe0eb;
-      border-radius: 8px;
-      font-weight: 400;
-      font-size: 14px;
-      color: #4b506d;
-      padding: 10px 16px;
-      max-width: 316px;
-      width: 100%;
-      outline: none;
-      &:hover {
-        border-color: #3751ff;
-      }
+  &.ant-input-affix-wrapper {
+    border: 1px solid #dfe0eb;
+    border-radius: 8px;
+    font-weight: 400;
+    font-size: 14px;
+    color: #4b506d;
+    padding: 10px 16px;
+    max-width: 316px;
+    width: 100%;
+    outline: none;
+    &:hover {
+      border-color: #3751ff;
     }
   }
 `;
