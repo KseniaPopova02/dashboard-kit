2083528e0708a282c2ffb0b544ca7c7088ac8737
Ksenia <kseniapopova02@gmail.com>
2023-04-12 19:29:36 +0200

    add: styles for Forgot

--- src/modal/Forgot/Forgot.jsx
+++ src/modal/Forgot/Forgot.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Logo, LogoTitle, LogoSubtitle, Question } from "../../components";
 import { ForgotForm } from "../ForgotForm";
+import { Container } from "../../components";
 
 export const Forgot = () => (
-  <>
+  <Container>
     <Logo />
     <LogoTitle>Forgot password?</LogoTitle>
     <LogoSubtitle>Enter your email from registered account</LogoSubtitle>
     <ForgotForm />
-  </>
+    <Question />
+  </Container>
 );
