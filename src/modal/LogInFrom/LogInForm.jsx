b8b8b9ffa2beb1c38ad92dcd6ac9265024bda1c7
Ksenia <kseniapopova02@gmail.com>
2023-04-12 13:55:04 +0200

    add: layout for LoginForm

---
+++ src/modal/LogInFrom/LogInForm.jsx
@@ -0,0 +1,36 @@
+import { Input, InputPassword, Button, Label } from "../../components";
+import { Formik, Form, Field, useFormik } from "formik";
+import { StyledForm } from "./style";
+
+export const LogInForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log("submit", values);
+    },
+  });
+  return (
+    <StyledForm onSubmit={formik.handleSubmit}>
+      <Label>Email</Label>
+      <Input
+        placeholder="email"
+        type="email"
+        name="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      <Label>Password</Label>
+      <InputPassword
+        name="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      <Button type="submit">Log In</Button>
+    </StyledForm>
+  );
+};
