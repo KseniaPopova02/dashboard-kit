fb1c9e58d3ac71caf1beabb5377fcc62c2cf4014
Ksenia <kseniapopova02@gmail.com>
2023-04-12 18:47:11 +0200

    add: logo for Forgot

--- src/modal/Forgot/Forgot.jsx
+++ src/modal/Forgot/Forgot.jsx
@@ -4,5 +4,7 @@ import { Logo, LogoTitle, LogoSubtitle, Question } from "../../components";
 export const Forgot = () => (
   <>
     <Logo />
+    <LogoTitle>Forgot password?</LogoTitle>
+    <LogoSubtitle>Enter your email from registered account</LogoSubtitle>
   </>
 );
