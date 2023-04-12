3d23dc099c9e2294599eed44619399d77424c1d1
Ksenia <kseniapopova02@gmail.com>
2023-04-12 14:06:40 +0200

    add: layout for router im App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import { GlobalStyle } from "./styles/globalStyles";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import {
+  Forgot,
+  ForgotSuccess,
+  LogIn,
+  Reset,
+  SignUp,
+  NotFound,
+  Layout,
+} from "./modal";
 
 const App = () => (
-  <>
-    <GlobalStyle />
-  </>
+  <BrowserRouter>
+    {/* <GlobalStyle /> */}
+
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Forgot />} />
+        <Route path="ForgotSuccess" element={<ForgotSuccess />} />
+        <Route path="LogIn" element={<LogIn />} />
+        <Route path="Reset" element={<Reset />} />
+        <Route path="SignUp" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
