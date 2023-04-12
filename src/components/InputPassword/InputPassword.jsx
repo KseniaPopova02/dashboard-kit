fbc9a221b8273d12220e13b006301ed57219edb7
Ksenia <kseniapopova02@gmail.com>
2023-04-12 13:59:02 +0200

    add: more props for InputPassword

--- src/components/InputPassword/InputPassword.jsx
+++ src/components/InputPassword/InputPassword.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { StyledInput } from "./style";
 
-export const InputPassword = ({ type = "text", placeholder = "" }) => (
-  <StyledInput placeholder={placeholder} type={type} />
+export const InputPassword = ({
+  type = "text",
+  placeholder = "",
+  name = "",
+  value = "",
+  onChange,
+}) => (
+  <StyledInput
+    placeholder={placeholder}
+    type={type}
+    name={name}
+    value={value}
+    onChange={onChange}
+  />
 );
