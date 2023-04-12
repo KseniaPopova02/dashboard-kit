703a812425773baf313b28ce08f7fd84f155412e
Ksenia <kseniapopova02@gmail.com>
2023-04-12 13:51:16 +0200

    delete: file

--- src/components/Label/Label.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledLabel } from "./style";
-
-export const Label = ({ children, htmlFor = "", className = "" }) => (
-  <StyledLabel htmlFor={htmlFor} className={className}>
-    {children}
-  </StyledLabel>
-);
