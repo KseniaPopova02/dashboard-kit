233cbe8c51807a1b70de4b08df71ea4a22222ba5
Ksenia <kseniapopova02@gmail.com>
2023-04-12 19:55:51 +0200

    clear: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { GlobalStyle } from "./styles/globalStyles";
 import { BrowserRouter } from "react-router-dom";
-import { LogIn } from "./modal";
 
 const App = () => (
   <BrowserRouter>
     <GlobalStyle />
-
-    <LogIn />
   </BrowserRouter>
 );
 
