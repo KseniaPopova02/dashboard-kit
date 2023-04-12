a82f3f650ed5189854de3497beaaac9ec818da30
Ksenia <kseniapopova02@gmail.com>
2023-04-12 13:54:27 +0200

    add: layout for LogIn

--- src/modal/LogIn/LogIn.jsx
+++ src/modal/LogIn/LogIn.jsx
@@ -1,3 +1,14 @@
 import React from "react";
+import { Logo, LogoTitle, LogoSubtitle, Question } from "../../components";
+import { LogInForm } from "../../modal";
+import { Container } from "../Forgot/style";
 
-export const LogIn = () => <></>;
+export const LogIn = () => (
+  <Container>
+    <Logo />
+    <LogoTitle>Log In to Dashboard Kit</LogoTitle>
+    <LogoSubtitle>Enter your email and password</LogoSubtitle>
+    <LogInForm />
+    <Question />
+  </Container>
+);
