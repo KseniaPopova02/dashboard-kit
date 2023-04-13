f21c8baf12e803ef0c8cf29dd5526e971d5bda1d
Ksenia <kseniapopova02@gmail.com>
2023-04-13 23:12:22 +0200

    add: some stuff in import

--- src/modules/LogInFrom/LogInForm.jsx
+++ src/modules/LogInFrom/LogInForm.jsx
@@ -6,7 +6,7 @@ import {
   Error,
   FormWrapper,
 } from "../../components";
-import { useFormik } from "formik";
+import { useFormik, Formik, Form, Field } from "formik";
 import { validationSchema } from "./schema";
 
 export const LogInForm = () => {
@@ -21,40 +21,46 @@ export const LogInForm = () => {
     validationSchema: validationSchema,
   });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <FormWrapper>
-        <Label>Email</Label>
-        <Input
-          placeholder="email"
-          type="email"
-          name="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.email && formik.errors.email ? (
-          <Error>{formik.errors.email}</Error>
-        ) : (
-          ""
-        )}
-        <Label>Password</Label>
-        <InputPassword
-          placeholder="password"
-          name="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.password && formik.errors.password ? (
-          <Error>{formik.errors.password}</Error>
-        ) : (
-          ""
-        )}
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={validationSchema}
+    >
+      <Form>
+        <Field placeholder="email" type="text" name="email" />
+        <FormWrapper>
+          <Label>Email</Label>
+          <Input
+            placeholder="email"
+            type="email"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <Error>{formik.errors.email}</Error>
+          ) : (
+            ""
+          )}
+          <Label>Password</Label>
+          <InputPassword
+            placeholder="password"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <Error>{formik.errors.password}</Error>
+          ) : (
+            ""
+          )}
 
-        <Button disabled={formik.isSubmitting} type="submit">
-          Log In
-        </Button>
-      </FormWrapper>
-    </form>
+          <Button disabled={formik.isSubmitting} type="submit">
+            Log In
+          </Button>
+        </FormWrapper>
+      </Form>
+    </Formik>
   );
 };
