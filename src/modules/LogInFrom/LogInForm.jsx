b686644cacae09c415701308cb6137a25f257327
Ksenia <kseniapopova02@gmail.com>
2023-04-14 00:40:46 +0200

    add: onSubmit function

--- src/modules/LogInFrom/LogInForm.jsx
+++ src/modules/LogInFrom/LogInForm.jsx
@@ -7,13 +7,20 @@ import {
 import { Formik, Form } from "formik";
 import { validationSchema } from "./schema";
 
+const onSubmit = async (values, actions) => {
+  console.log("values:", values);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+};
+
 export const LogInForm = () => {
   return (
     <Formik
       initialValues={{ email: "", password: "" }}
       validationSchema={validationSchema}
+      onSubmit={onSubmit}
     >
-      {(props) => (
+      {(isSubmitting) => (
         <Form>
           <FormWrapper>
             <CustomInput
@@ -22,8 +29,15 @@ export const LogInForm = () => {
               type="text"
               placeholder="Email"
             />
-
-            <Button type="submit">Log In</Button>
+            <CustomPasswordInput
+              label="Password"
+              name="password"
+              type="text"
+              placeholder="password"
+            />
+            <Button disabled={isSubmitting} type="submit">
+              Log In
+            </Button>
           </FormWrapper>
         </Form>
       )}
