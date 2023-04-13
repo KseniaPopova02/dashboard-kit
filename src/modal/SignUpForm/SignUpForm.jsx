655b801992a4f75cefeadc8617bc026d3665dc91
Ksenia <kseniapopova02@gmail.com>
2023-04-13 13:41:25 +0200

    add: validation for password

--- src/modal/SignUpForm/SignUpForm.jsx
+++ src/modal/SignUpForm/SignUpForm.jsx
@@ -25,7 +25,9 @@ export const SignUpForm = () => {
         .matches(/^[A-Za-z ]*$/, "Please enter valid surname")
         .max(40)
         .required(<Error>Surname is required</Error>),
-      password: Yup.string().required(<Error>Password is required</Error>),
+      repeatPassword: Yup.string()
+        .required(<Error>Password is required</Error>)
+        .oneOf([Yup.ref("password")], <Error>Passwords must match</Error>),
     }),
   });
   return (
