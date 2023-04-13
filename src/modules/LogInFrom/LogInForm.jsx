4314500202bd227e427017a2592542d6c7730ae2
Ksenia <kseniapopova02@gmail.com>
2023-04-14 00:07:26 +0200

    redo: Form on useField

--- src/modules/LogInFrom/LogInForm.jsx
+++ src/modules/LogInFrom/LogInForm.jsx
@@ -1,66 +1,26 @@
-import {
-  Input,
-  InputPassword,
-  Button,
-  Label,
-  Error,
-  FormWrapper,
-} from "../../components";
-import { useFormik, Formik, Form, Field } from "formik";
+import { Button, FormWrapper, CustomInput } from "../../components";
+import { Formik, Form } from "formik";
 import { validationSchema } from "./schema";
 
 export const LogInForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      console.log("submit", values);
-    },
-    validationSchema: validationSchema,
-  });
   return (
     <Formik
       initialValues={{ email: "", password: "" }}
       validationSchema={validationSchema}
     >
-      <Form>
-        <Field placeholder="email" type="text" name="email" />
-        <FormWrapper>
-          <Label>Email</Label>
-          <Input
-            placeholder="email"
-            type="email"
-            name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <Error>{formik.errors.email}</Error>
-          ) : (
-            ""
-          )}
-          <Label>Password</Label>
-          <InputPassword
-            placeholder="password"
-            name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.touched.password && formik.errors.password ? (
-            <Error>{formik.errors.password}</Error>
-          ) : (
-            ""
-          )}
-
-          <Button disabled={formik.isSubmitting} type="submit">
-            Log In
-          </Button>
-        </FormWrapper>
-      </Form>
+      {(props) => (
+        <Form>
+          <FormWrapper>
+            <CustomInput
+              label="Email"
+              name="email"
+              type="text"
+              placeholder="Email"
+            />
+            <Button type="submit">Log In</Button>
+          </FormWrapper>
+        </Form>
+      )}
     </Formik>
   );
 };
