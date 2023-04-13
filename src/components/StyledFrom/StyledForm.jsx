0cca746a5bb56b5b9c9b30e61514020a8df2b872
Ksenia <kseniapopova02@gmail.com>
2023-04-13 12:51:33 +0200

    rename: styledForm

--- src/components/StyledFrom/StyledForm.jsx
+++ src/components/StyledFrom/StyledForm.jsx
@@ -1,3 +1,3 @@
 import React from "react";
-import { StyledForm } from "./style";
-export const StyledForm = ({ children }) => <StyledForm>{children}</StyledForm>;
+import { Form } from "./style";
+export const StyledForm = ({ children }) => <Form>{children}</Form>;
