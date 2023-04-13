c2b3d14228159fc6065b54a6a6d0bbf829c08853
Ksenia <kseniapopova02@gmail.com>
2023-04-13 13:39:46 +0200

    add: min and max length for name and surname

--- src/modal/SignUpForm/SignUpForm.jsx
+++ src/modal/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,7 @@
-import { Input, InputPassword, Button, Label } from "../../components";
+import { Input, InputPassword, Button, Label, Error } from "../../components";
 import { useFormik } from "formik";
 import { StyledForm } from "./style";
+import * as Yup from "yup";
 
 export const SignUpForm = () => {
   const formik = useFormik({
@@ -14,6 +15,18 @@ export const SignUpForm = () => {
     onSubmit: (values) => {
       console.log("submit", values);
     },
+    validationSchema: Yup.object({
+      email: Yup.string().required(<Error>Email is required</Error>),
+      name: Yup.string()
+        .matches(/^[A-Za-z ]*$/, "Please enter valid name")
+        .max(40)
+        .required(<Error>Name is required</Error>),
+      surname: Yup.string()
+        .matches(/^[A-Za-z ]*$/, "Please enter valid surname")
+        .max(40)
+        .required(<Error>Surname is required</Error>),
+      password: Yup.string().required(<Error>Password is required</Error>),
+    }),
   });
   return (
     <StyledForm onSubmit={formik.handleSubmit}>
