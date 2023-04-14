f34db29f3c198be69dfc1c16375306919d15ccaa
Ksenia <kseniapopova02@gmail.com>
2023-04-14 12:50:20 +0200

    add: form layout

--- src/modules/SignUpForm/SignUpForm.jsx
+++ src/modules/SignUpForm/SignUpForm.jsx
@@ -1,95 +1,68 @@
 import {
-  Input,
-  InputPassword,
   Button,
-  Label,
-  Error,
-  StyledForm,
+  FormWrapper,
+  CustomInput,
+  CustomPasswordInput,
 } from "../../components";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import { Formik, Form } from "formik";
+import { validationSchema } from "./schema";
 
-export const SignUpForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      name: "",
-      surname: "",
-      password: "",
-      repeatPassword: "",
-    },
-    onSubmit: (values) => {
-      console.log("submit", values);
-    },
-    validationSchema: Yup.object({
-      email: Yup.string().required(<Error>Email is required</Error>),
-      name: Yup.string()
-        .matches(/^[A-Za-z ]*$/, "Please enter valid name")
-        .min(3)
-        .required(<Error>Name is required</Error>),
-      surname: Yup.string()
-        .matches(/^[A-Za-z ]*$/, "Please enter valid surname")
-        .min(3)
-        .required(<Error>Surname is required</Error>),
-      password: Yup.string().required(<Error>Password is required</Error>),
-      repeatPassword: Yup.string()
-        .required(<Error>Password is required</Error>)
-        .oneOf([Yup.ref("password")], <Error>Passwords must match</Error>),
-    }),
-  });
+const onSubmit = async (values, actions) => {
+  console.log("values:", values);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+};
+
+export const LogInForm = () => {
   return (
-    <StyledForm onSubmit={formik.handleSubmit}>
-      <Label>Email</Label>
-      <Input
-        placeholder="email"
-        type="email"
-        name="email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.email && formik.touched.email && formik.errors.email}
-      <Label>First name</Label>
-      <Input
-        placeholder="First name"
-        type="text"
-        name="name"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.name && formik.touched.name && formik.errors.name}
-      <Label>Last name</Label>
-      <Input
-        placeholder="Last name"
-        type="text"
-        name="surname"
-        value={formik.values.surname}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.surname && formik.touched.surname && formik.errors.surname}
-      <Label>Password</Label>
-      <InputPassword
-        name="password"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.password &&
-        formik.touched.password &&
-        formik.errors.password}
-      <Label>confirm password</Label>
-      <InputPassword
-        name="repeatPassword"
-        value={formik.values.repeatPassword}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.repeatPassword &&
-        formik.touched.repeatPassword &&
-        formik.errors.repeatPassword}
-      <Button type="submit">Register</Button>
-    </StyledForm>
+    <Formik
+      initialValues={{
+        email: "",
+        name: "",
+        surname: "",
+        password: "",
+        repeatPassword: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(props) => (
+        <Form>
+          <FormWrapper>
+            <CustomInput
+              label="Email"
+              name="email"
+              type="text"
+              placeholder="Email"
+            />
+            <CustomInput
+              label="First name"
+              name="name"
+              type="text"
+              placeholder="First name"
+            />
+            <CustomInput
+              label="Last name"
+              name="surname"
+              type="text"
+              placeholder="Last name"
+            />
+            <CustomPasswordInput
+              label="Password"
+              name="password"
+              type="text"
+              placeholder="password"
+            />
+            <CustomPasswordInput
+              label="confirm password"
+              name="confirmPassword"
+              type="text"
+              placeholder="password"
+            />
+            <Button type="submit">Register</Button>
+          </FormWrapper>
+        </Form>
+      )}
+    </Formik>
   );
 };
