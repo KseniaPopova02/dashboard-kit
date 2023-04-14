00eb1e2c32c34f7b503b0858e3f608a6b18e06f7
Ksenia <kseniapopova02@gmail.com>
2023-04-14 14:13:09 +0200

    fix: typo

--- src/modules/Layout/Layout.jsx
+++ src/modules/Layout/Layout.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import { Container, FormWrapper } from "../../components";
+import { Container } from "../../components";
 
 export const Layout = () => (
   <Container>
-    <FormWrapper>
-      <Outlet />
-    </FormWrapper>
+    <Outlet />
   </Container>
 );
