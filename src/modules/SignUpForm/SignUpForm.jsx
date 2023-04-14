facf52b59ef6432dc54535a6c6908dae3a3f1a12
Ksenia <kseniapopova02@gmail.com>
2023-04-14 18:09:46 +0200

    add: direct return

--- src/modules/SignUpForm/SignUpForm.jsx
+++ src/modules/SignUpForm/SignUpForm.jsx
@@ -13,56 +13,54 @@ const onSubmit = async (values, actions) => {
   actions.resetForm();
 };
 
-export const SignUpForm = () => {
-  return (
-    <Formik
-      initialValues={{
-        email: "",
-        name: "",
-        surname: "",
-        password: "",
-        repeatPassword: "",
-      }}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      {(props) => (
-        <Form>
-          <FormWrapper>
-            <CustomInput
-              label="Email"
-              name="email"
-              type="text"
-              placeholder="Email"
-            />
-            <CustomInput
-              label="First name"
-              name="name"
-              type="text"
-              placeholder="First name"
-            />
-            <CustomInput
-              label="Last name"
-              name="surname"
-              type="text"
-              placeholder="Last name"
-            />
-            <CustomPasswordInput
-              label="Password"
-              name="password"
-              type="text"
-              placeholder="password"
-            />
-            <CustomPasswordInput
-              label="confirm password"
-              name="confirmPassword"
-              type="text"
-              placeholder="password"
-            />
-            <Button type="submit">Register</Button>
-          </FormWrapper>
-        </Form>
-      )}
-    </Formik>
-  );
-};
+export const SignUpForm = () => (
+  <Formik
+    initialValues={{
+      email: "",
+      name: "",
+      surname: "",
+      password: "",
+      repeatPassword: "",
+    }}
+    validationSchema={validationSchema}
+    onSubmit={onSubmit}
+  >
+    {(props) => (
+      <Form>
+        <FormWrapper>
+          <CustomInput
+            label="Email"
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+          <CustomInput
+            label="First name"
+            name="name"
+            type="text"
+            placeholder="First name"
+          />
+          <CustomInput
+            label="Last name"
+            name="surname"
+            type="text"
+            placeholder="Last name"
+          />
+          <CustomPasswordInput
+            label="Password"
+            name="password"
+            type="text"
+            placeholder="password"
+          />
+          <CustomPasswordInput
+            label="confirm password"
+            name="confirmPassword"
+            type="text"
+            placeholder="password"
+          />
+          <Button type="submit">Register</Button>
+        </FormWrapper>
+      </Form>
+    )}
+  </Formik>
+);
