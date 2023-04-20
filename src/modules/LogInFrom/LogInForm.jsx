778efde12f3860d4ff6a5718bc6841b2a5a6779d
Ksenia <kseniapopova02@gmail.com>
2023-04-20 18:14:08 +0200

    add: mocked data

--- src/modules/LogInFrom/LogInForm.jsx
+++ src/modules/LogInFrom/LogInForm.jsx
@@ -1,18 +1,32 @@
 import { FormContent } from "./FormContent";
 import { Formik } from "formik";
 import { initialValues, validationSchema } from "./formConfig";
+import { useNavigate } from "react-router-dom";
 
-const onSubmit = async (actions) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  actions.resetForm();
+export const LogInForm = () => {
+  const navigate = useNavigate();
+  const onSubmit = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const mockedEmail = "anton@gmali.com";
+    const mockedPassword = "AntonLuchiiMentorVMire228";
+
+    if (values.email === mockedEmail && values.password === mockedPassword) {
+      navigate("/dashboard");
+    } else {
+      actions.setFieldError("email", "Wrong email or password");
+      actions.setFieldError("password", "Wrong email or password");
+    }
+
+    actions.setSubmitting(false);
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {() => <FormContent />}
+    </Formik>
+  );
 };
-
-export const LogInForm = () => (
-  <Formik
-    initialValues={initialValues}
-    validationSchema={validationSchema}
-    onSubmit={onSubmit}
-  >
-    {() => <FormContent />}
-  </Formik>
-);
