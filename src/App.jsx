55a624b3dc3170bf3b0c7b276b5b2a1d97678746
Ksenia <kseniapopova02@gmail.com>
2023-04-20 13:29:15 +0200

    add: routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
 import { GlobalStyle } from "./styles/globalStyles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Forgot, ForgotSuccess, LogIn, Reset, SignUp, NotFound } from "./pages";
-import { Layout } from "./modules";
+import {
+  Forgot,
+  ForgotSuccess,
+  LogIn,
+  Reset,
+  SignUp,
+  NotFound,
+  View,
+  Tickets,
+  Ideas,
+  Contacts,
+  Agents,
+  Articles,
+  Settings,
+  Subscription,
+} from "./pages";
+import { FormLayout, MainLayout } from "./modules";
 
-const App = () => (
-  <BrowserRouter>
-    <GlobalStyle />
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<LogIn />} />
-        <Route path="forgot" element={<Forgot />} />
-        <Route path="forgotSuccess" element={<ForgotSuccess />} />
-        <Route path="reset" element={<Reset />} />
-        <Route path="signUp" element={<SignUp />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  const loggedIn = true;
+  return (
+    <BrowserRouter>
+      <GlobalStyle loggedIn={loggedIn} />
+      <Routes>
+        {loggedIn ? (
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<View />} />
+            <Route path="Tickets" element={<Tickets />} />
+            <Route path="Ideas" element={<Ideas />} />
+            <Route path="Contacts" element={<Contacts />} />
+            <Route path="Agents" element={<Agents />} />
+            <Route path="Articles" element={<Articles />} />
+            <Route path="Settings" element={<Settings />} />
+            <Route path="Subscription" element={<Subscription />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        ) : (
+          <Route path="/" element={<FormLayout />}>
+            <Route index element={<LogIn />} />
+            <Route path="forgot" element={<Forgot />} />
+            <Route path="forgotSuccess" element={<ForgotSuccess />} />
+            <Route path="reset" element={<Reset />} />
+            <Route path="signUp" element={<SignUp />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
