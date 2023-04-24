93d71f2492f2fe3f35f6f2167e2deed04721daa5
Ksenia <kseniapopova02@gmail.com>
2023-04-24 16:42:50 +0200

    remove: checking

--- src/modules/SignUpForm/SignUpForm.jsx
+++ src/modules/SignUpForm/SignUpForm.jsx
@@ -2,7 +2,6 @@ import { Formik } from "formik";
 import { validationSchema, initialValues } from "./formConfig";
 import { FormContent } from "./FormContent";
 import { useNavigate } from "react-router-dom";
-import userData from "../../Data/userData.json";
 
 export const SignUpForm = () => {
   const navigate = useNavigate();
@@ -10,20 +9,7 @@ export const SignUpForm = () => {
   const onSubmit = async (values, actions) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log(values);
-    const { mockedEmail, mockedName, mockedLastName, mockedPassword } =
-      userData;
-
-    if (
-      values.email === mockedEmail &&
-      values.name === mockedName &&
-      values.surname === mockedLastName &&
-      values.password === mockedPassword
-    ) {
-      navigate("/dashboard");
-    } else {
-      alert("Something went wrong, sorry");
-    }
-
+    navigate("/dashboard");
     actions.setSubmitting(false);
   };
   return (
