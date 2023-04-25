a17b95d6f548f5d69e5dd3705b5d792c9cf340b2
Ksenia <kseniapopova02@gmail.com>
2023-04-25 18:05:32 +0200

    resolve: merge conflicts

--- src/modules/LogInFrom/LogInForm.jsx
+++ src/modules/LogInFrom/LogInForm.jsx
@@ -2,7 +2,6 @@ import { FormContent } from "./FormContent";
 import { Formik } from "formik";
 import { initialValues, validationSchema } from "./formConfig";
 import { useNavigate } from "react-router-dom";
-import userData from "../../Data/userData.json";
 
 export const LogInForm = ({ updateLoggedIn }) => {
   const navigate = useNavigate();
@@ -10,7 +9,6 @@ export const LogInForm = ({ updateLoggedIn }) => {
   const onSubmit = async (values, actions) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-<<<<<<< HEAD
     const users = JSON.parse(localStorage.getItem("users")) || [];
 
     const user = users.find(
@@ -20,12 +18,6 @@ export const LogInForm = ({ updateLoggedIn }) => {
     if (user) {
       updateLoggedIn(true);
       navigate("/dashboard/Overview");
-=======
-    const { userEmail, userPassword } = userData;
-
-    if (values.email === userEmail && values.password === userPassword) {
-      navigate("/dashboard");
->>>>>>> 32e26ed (remove: mocked data into different file)
     } else {
       actions.setFieldError("email", "Wrong email or password");
       actions.setFieldError("password", "Wrong email or password");
