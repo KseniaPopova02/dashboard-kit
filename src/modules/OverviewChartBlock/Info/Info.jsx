7ffd014da994003f9543ed3b9027f6fed13d396b
Ksenia <kseniapopova02@gmail.com>
2023-04-27 20:30:32 +0200

    add: formated function

--- src/modules/OverviewChartBlock/Info/Info.jsx
+++ src/modules/OverviewChartBlock/Info/Info.jsx
@@ -1,6 +1,32 @@
 import { React, useState, useEffect } from "react";
 import data from "../../../Data/OverviewChart.json";
 
+const formatData = (dataChart) => {
+  return Object.entries(dataChart).map(([key, value]) => {
+    let formattedValue = value;
+    if (key === "Average response time") {
+      formattedValue = Math.floor(value / 60) + "h " + (value % 60) + "m";
+      return [key, formattedValue];
+    }
+
+    if (key === "Average first response time") {
+      if (value >= 60) {
+        formattedValue = Math.floor(value / 60) + "h " + (value % 60) + "m";
+        return [key, formattedValue];
+      } else {
+        formattedValue = `${value}m`;
+        return [key, formattedValue];
+      }
+    }
+    if (key === "Resolution within SLA") {
+      formattedValue = `${value}%`;
+      return [key, formattedValue];
+    } else {
+      return [key, value];
+    }
+  });
+};
+
 export const Info = () => {
   const [dataChart, setDataChart] = useState([]);
   useEffect(() => {
@@ -8,7 +34,7 @@ export const Info = () => {
   }, []);
   return (
     <div>
-      {Object.entries(dataChart).map(([key, value]) => (
+      {formatData(dataChart).map(([key, value]) => (
         <div key={key}>
           <div>{key}</div>
           <div>{value}</div>
