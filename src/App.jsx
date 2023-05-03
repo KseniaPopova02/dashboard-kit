8a05a96a56aaa4176767befdb93544d8ab6aabd4
Ksenia <kseniapopova02@gmail.com>
2023-05-03 17:25:02 +0200

    fix: routes

--- src/App.jsx
+++ src/App.jsx
@@ -30,23 +30,22 @@ const App = () => {
       <GlobalStyle loggedIn={loggedIn} />
       <Routes>
         <Route path="/dashboard" element={<MainLayout loggedIn={loggedIn} />}>
-          <Route path="Overview" element={<View />} />
-          <Route path="Tickets" element={<Tickets />} />
-          <Route path="Ideas" element={<Ideas />} />
-          <Route path="Contacts" element={<Contacts />} />
-          <Route path="Agents" element={<Agents />} />
-          <Route path="Articles" element={<Articles />} />
-          <Route path="Settings" element={<Settings />} />
-          <Route path="Subscription" element={<Subscription />} />
+          <Route path="/overview" element={<View />} />
+          <Route path="/tickets" element={<Tickets />} />
+          <Route path="/ideas" element={<Ideas />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/agents" element={<Agents />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/subscription" element={<Subscription />} />
           <Route path="*" element={<NotFound />} />
         </Route>
-
         <Route path="/" element={<FormLayout />}>
           <Route index element={<LogIn updateLoggedIn={updateLoggedIn} />} />
-          <Route path="forgot" element={<Forgot />} />
-          <Route path="reset" element={<Reset />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/reset" element={<Reset />} />
           <Route
-            path="sign-up"
+            path="/sign-up"
             element={<SignUp updateLoggedIn={updateLoggedIn} />}
           />
           <Route path="*" element={<NotFound />} />
