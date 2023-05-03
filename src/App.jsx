20215321bd4f9e57fb18a386ac07ff7337093741
Ksenia <kseniapopova02@gmail.com>
2023-05-03 17:12:02 +0200

    fix: authorizing check

--- src/App.jsx
+++ src/App.jsx
@@ -29,30 +29,28 @@ const App = () => {
     <BrowserRouter>
       <GlobalStyle loggedIn={loggedIn} />
       <Routes>
-        {loggedIn ? (
-          <Route path="/dashboard" element={<MainLayout />}>
-            <Route path="Overview" element={<View />} />
-            <Route path="Tickets" element={<Tickets />} />
-            <Route path="Ideas" element={<Ideas />} />
-            <Route path="Contacts" element={<Contacts />} />
-            <Route path="Agents" element={<Agents />} />
-            <Route path="Articles" element={<Articles />} />
-            <Route path="Settings" element={<Settings />} />
-            <Route path="Subscription" element={<Subscription />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        ) : (
-          <Route path="/" element={<FormLayout />}>
-            <Route index element={<LogIn updateLoggedIn={updateLoggedIn} />} />
-            <Route path="forgot" element={<Forgot />} />
-            <Route path="reset" element={<Reset />} />
-            <Route
-              path="sign-up"
-              element={<SignUp updateLoggedIn={updateLoggedIn} />}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        )}
+        <Route path="/dashboard" element={<MainLayout loggedIn={loggedIn} />}>
+          <Route path="Overview" element={<View />} />
+          <Route path="Tickets" element={<Tickets />} />
+          <Route path="Ideas" element={<Ideas />} />
+          <Route path="Contacts" element={<Contacts />} />
+          <Route path="Agents" element={<Agents />} />
+          <Route path="Articles" element={<Articles />} />
+          <Route path="Settings" element={<Settings />} />
+          <Route path="Subscription" element={<Subscription />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+
+        <Route path="/" element={<FormLayout />}>
+          <Route index element={<LogIn updateLoggedIn={updateLoggedIn} />} />
+          <Route path="forgot" element={<Forgot />} />
+          <Route path="reset" element={<Reset />} />
+          <Route
+            path="sign-up"
+            element={<SignUp updateLoggedIn={updateLoggedIn} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
