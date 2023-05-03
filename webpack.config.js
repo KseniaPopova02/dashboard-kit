0c423007b02858441033d4f48055545d11bbdfa9
Ksenia <kseniapopova02@gmail.com>
2023-05-03 12:56:26 +0200

    install:  @svgr

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgo: false,
+            },
+          },
+          "url-loader",
+        ],
+      },
+    ],
+  },
+};
