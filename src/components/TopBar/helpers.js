5485b985135f4035540e1f020873b72197b13439
Ksenia <kseniapopova02@gmail.com>
2023-05-05 16:54:57 +0300

    add: new gettitle function

--- src/components/TopBar/helpers.js
+++ src/components/TopBar/helpers.js
@@ -1,22 +1,58 @@
+// import { ROUTES } from "../../Routes/routes";
+
+// export const getTitle = (pathname) => {
+//   const {
+//     DASHBOARD,
+//     OVERVIEW,
+//     TICKETS,
+//     IDEAS,
+//     CONTACTS,
+//     AGENTS,
+//     ARTICLES,
+//     SETTINGS,
+//     SUBSCRIPTION,
+//   } = ROUTES;
+
+//   switch (pathname.toLowerCase()) {
+//     case `${DASHBOARD}/${OVERVIEW}`:
+//       return "Overview";
+//     case `${DASHBOARD}/${TICKETS}`:
+//       return "Tickets";
+//     case `${DASHBOARD}/${IDEAS}`:
+//       return "Ideas";
+//     case `${DASHBOARD}/${CONTACTS}`:
+//       return "Contacts";
+//     case `${DASHBOARD}/${AGENTS}`:
+//       return "Agents";
+//     case `${DASHBOARD}/${ARTICLES}`:
+//       return "Articles";
+//     case `${DASHBOARD}/${SETTINGS}`:
+//       return "Settings";
+//     case `${DASHBOARD}/${SUBSCRIPTION}`:
+//       return "Subscription";
+//     default:
+//       return "404 Not Found";
+//   }
+// };
+
+import { ROUTES } from "../../Routes/routes";
+
+const routeTitles = {
+  [ROUTES.OVERVIEW]: "Overview",
+  [ROUTES.TICKETS]: "Tickets",
+  [ROUTES.IDEAS]: "Ideas",
+  [ROUTES.CONTACTS]: "Contacts",
+  [ROUTES.AGENTS]: "Agents",
+  [ROUTES.ARTICLES]: "Articles",
+  [ROUTES.SETTINGS]: "Settings",
+  [ROUTES.SUBSCRIPTION]: "Subscription",
+};
+
 export const getTitle = (pathname) => {
-  switch (pathname) {
-    case "/dashboard/Overview":
-      return "Overview";
-    case "/dashboard/Tickets":
-      return "Tickets";
-    case "/dashboard/Ideas":
-      return "Ideas";
-    case "/dashboard/Contacts":
-      return "Contacts";
-    case "/dashboard/Agents":
-      return "Agents";
-    case "/dashboard/Articles":
-      return "Articles";
-    case "/dashboard/Settings":
-      return "Settings";
-    case "/dashboard/Subscription":
-      return "Subscription";
-    default:
-      return "Error";
-  }
+  const { DASHBOARD } = ROUTES;
+  const path = pathname.replace(`${DASHBOARD}/`, "").toLowerCase();
+  const title = routeTitles[`${path}`];
+  console.log(path);
+  console.log(title);
+  return title || "Error 404";
 };
