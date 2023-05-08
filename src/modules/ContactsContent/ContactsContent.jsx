33108a82b603f2259096cae428d9200af96e69d2
Ksenia <kseniapopova02@gmail.com>
2023-05-08 14:49:36 +0300

    add: ability to add contacts

--- src/modules/ContactsContent/ContactsContent.jsx
+++ src/modules/ContactsContent/ContactsContent.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Formik } from "formik";
 
 import { contactSchema, initialValues, FormContent } from "./Form";
 
 export const ContactsContent = () => {
   const [contacts, setContacts] = useState([]);
-  const [sortType, setSortType] = useState("asc");
   const [showForm, setShowForm] = useState(false);
-  const [filterBy, setFilterBy] = useState("");
-  const [filterValue, setFilterValue] = useState("");
+  const [displayContacts, setDisplayContacts] = useState(8);
+
+  useEffect(() => {
+    const storedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (storedContacts) {
+      setContacts(storedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleAddContact = (values, { resetForm }) => {
     const newContact = {
@@ -19,79 +28,46 @@ export const ContactsContent = () => {
       email: values.email,
       address: values.address,
     };
-    setContacts([...contacts, newContact]);
+    setContacts((prevContacts) => [newContact, ...prevContacts]);
     setShowForm(false);
     resetForm();
+    console.log("Submitted");
   };
-
-  const handleSortContacts = () => {
-    const sortedContacts = [...contacts].sort((a, b) => {
-      if (sortType === "asc") {
-        return a.firstName.localeCompare(b.firstName);
-      } else {
-        return b.firstName.localeCompare(a.firstName);
-      }
-    });
-    setContacts(sortedContacts);
-    setSortType(sortType === "asc" ? "desc" : "asc");
-  };
-
-  const handleFilterBy = (param) => {
-    setFilterBy(param);
-    setFilterValue("");
-  };
-
-  const handleFilterChange = (event) => {
-    setFilterValue(event.target.value);
-  };
-
-  const filteredContacts = contacts
-    .filter((contact) =>
-      filterBy
-        ? contact[filterBy].toLowerCase().includes(filterValue.toLowerCase())
-        : true
-    )
-    .sort((a, b) => a[filterBy].localeCompare(b[filterBy]));
 
   return (
     <div>
       <div>
-        {filterBy ? (
-          <input
-            type="text"
-            value={filterValue}
-            onChange={handleFilterChange}
-          />
-        ) : (
-          <button onClick={() => handleFilterBy("firstName")}>Filter</button>
-        )}
-        <button onClick={() => handleFilterBy("")}>Clear filter</button>
-        <button onClick={handleSortContacts}>Sort</button>
         <button onClick={() => setShowForm(true)}>Add contact</button>
       </div>
       {showForm && (
         <Formik
           initialValues={initialValues}
-          onSubmit={handleAddContact}
           validationSchema={contactSchema}
+          onSubmit={handleAddContact}
         >
-          {() => <FormContent setShowForm={setShowForm} />}
+          {({ handleSubmit }) => (
+            <FormContent onSubmit={handleSubmit} setShowForm={setShowForm} />
+          )}
         </Formik>
       )}
-      <ul>
-        {filteredContacts.map((contact) => (
-          <li key={contact.id}>
-            <div>
-              <img src={contact.photo} alt={contact.firstName} />
+      {contacts.length > 0 ? (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
               <div>
-                {contact.firstName} {contact.lastName}
+                <img src={contact.photo} alt={contact.firstName} />
+                <div>
+                  {contact.firstName} {contact.lastName}
+                </div>
+                <div>{contact.email}</div>
+                <div>{contact.address}</div>
               </div>
-              <div>{contact.email}</div>
-              <div>{contact.address}</div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>No contacts yet</div>
+      )}
     </div>
   );
 };
--- src/modules/ContactsContent/Form/FormContent.jsx
+++ src/modules/ContactsContent/Form/FormContent.jsx
@@ -1,29 +1,29 @@
-import { Field } from "formik";
 import {
   CustomInput,
   Button,
   StyledFormWrapper,
   LogoTitle,
 } from "../../../components";
+
 import { ModalWrapper, ModalOverlay, StyledForm, StyledBtn } from "./style";
 
-export const FormContent = ({ setShowForm }) => (
+export const FormContent = ({ setShowForm, onSubmit }) => (
   <>
     <ModalOverlay />
     <ModalWrapper>
-      <StyledForm>
+      <StyledForm onSubmit={onSubmit}>
         <StyledFormWrapper>
           <LogoTitle>Add new contact</LogoTitle>
-          <Field type="text" name="photo" placeholder="Photo" />
+
           <CustomInput
             label="First name"
-            name="name"
+            name="firstName"
             type="text"
             placeholder="First name"
           />
           <CustomInput
             label="Last name"
-            name="surname"
+            name="lastName"
             type="text"
             placeholder="Last name"
           />
