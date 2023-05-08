fea96d7fdd391c6b5012d3350ee3ec8b2bb11431
Ksenia <kseniapopova02@gmail.com>
2023-05-08 09:39:10 +0300

    add: Yup schema

---
+++ src/modules/ContactsContent/Form/formConfig.js
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+export const contactSchema = Yup.object().shape({
+  photo: Yup.string(),
+  firstName: Yup.string()
+    .matches(/^[A-Za-z ]*$/, "Please enter valid first name")
+    .min(3)
+    .required("First name is required"),
+  lastName: Yup.string()
+    .matches(/^[A-Za-z ]*$/, "Please enter valid Last name")
+    .min(3)
+    .required("Last name is required"),
+  email: Yup.string()
+    .email("Please input a valid email")
+    .required("Email is required"),
+  address: Yup.string(),
+});
