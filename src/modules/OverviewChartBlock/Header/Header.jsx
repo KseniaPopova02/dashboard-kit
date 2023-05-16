8c011485c553b42e6fa076911cde0fa47d508916
Ksenia <kseniapopova02@gmail.com>
2023-05-16 18:23:32 +0300

    add: formatting time and date function

--- src/modules/OverviewChartBlock/Header/Header.jsx
+++ src/modules/OverviewChartBlock/Header/Header.jsx
@@ -1,8 +1,47 @@
 import { StyledHeaderWrapper, StyledSubtitle, StyledTitle } from "./style";
 
-export const Header = () => (
-  <StyledHeaderWrapper>
-    <StyledTitle>Today’s trends</StyledTitle>
-    <StyledSubtitle>as of 25 May 2019, 09:41 PM</StyledSubtitle>
-  </StyledHeaderWrapper>
-);
+function formatDate(date) {
+  function getDayOrdinal(day) {
+    if (day > 3 && day < 21) {
+      return `${day}th`;
+    }
+    switch (day % 10) {
+      case 1:
+        return `${day}st`;
+      case 2:
+        return `${day}nd`;
+      case 3:
+        return `${day}rd`;
+      default:
+        return `${day}th`;
+    }
+  }
+
+  function formatTime(time) {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const amPm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    return `${formattedHours}:${formattedMinutes} ${amPm}`;
+  }
+
+  const day = date.getDate();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+  const time = formatTime(date);
+
+  return `as of ${getDayOrdinal(day)} ${month} ${year}, ${time}`;
+}
+
+export const Header = () => {
+  const currentDate = new Date();
+  const formattedDate = formatDate(currentDate);
+
+  return (
+    <StyledHeaderWrapper>
+      <StyledTitle>Today’s trends</StyledTitle>
+      <StyledSubtitle>{formattedDate}</StyledSubtitle>
+    </StyledHeaderWrapper>
+  );
+};
