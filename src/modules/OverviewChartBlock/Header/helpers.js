43d9a15e7908ad2a85b86759cfd9d8853dd081d9
Ksenia <kseniapopova02@gmail.com>
2023-05-18 22:52:42 +0300

    take out: functions form another function

--- src/modules/OverviewChartBlock/Header/helpers.js
+++ src/modules/OverviewChartBlock/Header/helpers.js
@@ -1,29 +1,29 @@
-export const formatDate = (date) => {
-  const getDayOrdinal = (day) => {
-    if (day > 3 && day < 21) {
+const formatTime = (time) => {
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const amPm = hours >= 12 ? "PM" : "AM";
+  const formattedHours = hours % 12 || 12;
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  return `${formattedHours}:${formattedMinutes} ${amPm}`;
+};
+
+const getDayOrdinal = (day) => {
+  if (day > 3 && day < 21) {
+    return `${day}th`;
+  }
+  switch (day % 10) {
+    case 1:
+      return `${day}st`;
+    case 2:
+      return `${day}nd`;
+    case 3:
+      return `${day}rd`;
+    default:
       return `${day}th`;
-    }
-    switch (day % 10) {
-      case 1:
-        return `${day}st`;
-      case 2:
-        return `${day}nd`;
-      case 3:
-        return `${day}rd`;
-      default:
-        return `${day}th`;
-    }
-  };
+  }
+};
 
-  const formatTime = (time) => {
-    const hours = time.getHours();
-    const minutes = time.getMinutes();
-    const amPm = hours >= 12 ? "PM" : "AM";
-    const formattedHours = hours % 12 || 12;
-    const formattedMinutes = String(minutes).padStart(2, "0");
-    return `${formattedHours}:${formattedMinutes} ${amPm}`;
-  };
-
+export const formatDate = (date) => {
   const day = date.getDate();
   const month = date.toLocaleString("en-US", { month: "long" });
   const year = date.getFullYear();
