baaabd9ff38c0768124d3fd1aec916ed7105f535
Ksenia <kseniapopova02@gmail.com>
2023-05-18 22:23:26 +0300

    remove: configs into different folder

--- src/modules/OverviewChartBlock/Chart/Chart.jsx
+++ src/modules/OverviewChartBlock/Chart/Chart.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { StyledLine } from "./style";
 import { Header } from "../Header";
-import { CustomDot } from "./CustomDot";
+import { configChart } from "./config";
 import chartData from "../../../Data/Chart.json";
 
 export const Chart = () => {
@@ -17,83 +17,7 @@ export const Chart = () => {
 
   const config = {
     data,
-    xField: "x",
-    yField: "y",
-    seriesField: "category",
-    point: {
-      visible: false,
-      shape: {
-        customContent: (title, items) => {
-          return (
-            <div>
-              {title && <div>{title}</div>}
-              {items.map((item, index) => (
-                <CustomDot
-                  key={index}
-                  x={item.x}
-                  y={item.y}
-                  category={item.category}
-                />
-              ))}
-            </div>
-          );
-        },
-      },
-    },
-    xAxis: {
-      lines: false,
-      type: "linear",
-      tickInterval: 1,
-      min: 0,
-      max: 22,
-      tickLine: null,
-      line: {
-        style: {
-          stroke: "#EBEDF0",
-        },
-      },
-      label: {
-        style: {
-          fill: "#9FA2B4",
-        },
-      },
-    },
-    yAxis: {
-      position: "right",
-      type: "linear",
-      tickInterval: 10,
-      min: 0,
-      max: 60,
-      label: {
-        style: {
-          fill: "#9FA2B4",
-        },
-      },
-      grid: {
-        line: {
-          style: {
-            stroke: "#EBEDF0",
-          },
-        },
-      },
-    },
-    color: ["#3751FF", "#DFE0EB"],
-    legend: {
-      position: "top-right",
-    },
-    smooth: true,
-    tooltip: {
-      position: "top",
-    },
-    // series: [
-    //   {
-    //     type: "line",
-    //     name: "today",
-    //     areaStyle: {
-    //       fill: "l(270) 0:#ffffff 0.5:#7ec2f3 1:#1890ff",
-    //     },
-    //   },
-    // ],
+    ...configChart,
   };
 
   return (
