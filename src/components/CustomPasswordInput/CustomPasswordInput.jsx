7ede7f142e29e0bfc8e775337f891d2dbf8a4cb1
Ksenia <kseniapopova02@gmail.com>
2023-05-18 22:03:05 +0300

    remove: cyclic dependency

--- src/components/CustomPasswordInput/CustomPasswordInput.jsx
+++ src/components/CustomPasswordInput/CustomPasswordInput.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useField } from "formik";
 import { StyledWrapper } from "./style";
-import { Label, InputPassword, Error } from "..";
+import { Label } from "../Label";
+import { InputPassword } from "../InputPassword";
+import { Error } from "../Error";
 
 export const CustomPasswordInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
