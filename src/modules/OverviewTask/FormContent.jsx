1897d3ca739157227007751402d9ebe2f1866971
Ksenia <kseniapopova02@gmail.com>
2023-05-19 15:09:56 +0300

    replace: customInput on ordinary StyledInput

--- src/modules/OverviewTask/FormContent.jsx
+++ src/modules/OverviewTask/FormContent.jsx
@@ -1,5 +1,4 @@
-import { Form, Field } from "formik";
-import { CustomInput } from "./CastomInput";
+import { Form, Field, useFormikContext } from "formik";
 import {
   StyledBtn,
   StyledInputWrapper,
@@ -11,43 +10,58 @@ import {
   StyledBin,
   StyledAdd,
   StyledBtnWrapper,
+  StyledInput,
 } from "./style";
 
-export const FormContent = ({ handleDeleteAllTasks }) => (
-  <Form>
-    <StyledFormWrapper>
-      <StyledInputWrapper>
-        <div>
-          <CustomInput
-            name="taskName"
-            type="text"
-            placeholder="Create new task"
-          />
-        </div>
+export const FormContent = ({ handleDeleteAllTasks }) => {
+  const { values, setFieldValue } = useFormikContext();
 
-        <StyledBtnWrapper>
-          <StyledBtn type="submit">
-            <StyledAdd />
-          </StyledBtn>
-          <StyledBtn type="button" onClick={handleDeleteAllTasks}>
-            <StyledBin />
-          </StyledBtn>
-        </StyledBtnWrapper>
-      </StyledInputWrapper>
-      <StyledRadioWrapper>
-        <div>
-          <Field type="radio" id="urgent" name="flags" value="urgent" />
-          <StyledLabelUrgent htmlFor="urgent">Urgent</StyledLabelUrgent>
-        </div>
-        <div>
-          <Field type="radio" id="new" name="flags" value="new" />
-          <StyledLabelNew htmlFor="new">New</StyledLabelNew>
-        </div>
-        <div>
-          <Field type="radio" id="default" name="flags" value="default" />
-          <StyledLabelDefault htmlFor="default">Default</StyledLabelDefault>
-        </div>
-      </StyledRadioWrapper>
-    </StyledFormWrapper>
-  </Form>
-);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const trimmedValue = value.trim();
+    if (trimmedValue || value === "") {
+      setFieldValue(name, value);
+    }
+  };
+
+  return (
+    <Form>
+      <StyledFormWrapper>
+        <StyledInputWrapper>
+          <div>
+            <StyledInput
+              name="taskName"
+              type="text"
+              placeholder="Create new task"
+              value={values.taskName}
+              onChange={handleChange}
+            />
+          </div>
+
+          <StyledBtnWrapper>
+            <StyledBtn type="submit">
+              <StyledAdd />
+            </StyledBtn>
+            <StyledBtn type="button" onClick={handleDeleteAllTasks}>
+              <StyledBin />
+            </StyledBtn>
+          </StyledBtnWrapper>
+        </StyledInputWrapper>
+        <StyledRadioWrapper>
+          <div>
+            <Field type="radio" id="urgent" name="flags" value="urgent" />
+            <StyledLabelUrgent htmlFor="urgent">Urgent</StyledLabelUrgent>
+          </div>
+          <div>
+            <Field type="radio" id="new" name="flags" value="new" />
+            <StyledLabelNew htmlFor="new">New</StyledLabelNew>
+          </div>
+          <div>
+            <Field type="radio" id="default" name="flags" value="default" />
+            <StyledLabelDefault htmlFor="default">Default</StyledLabelDefault>
+          </div>
+        </StyledRadioWrapper>
+      </StyledFormWrapper>
+    </Form>
+  );
+};
