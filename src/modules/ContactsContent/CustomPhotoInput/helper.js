4ebe63dee70dd4ef05ff178c4abba2945eab9919
Ksenia <kseniapopova02@gmail.com>
2023-05-30 18:36:19 +0300

    remove: commented code

--- src/modules/ContactsContent/CustomPhotoInput/helper.js
+++ src/modules/ContactsContent/CustomPhotoInput/helper.js
@@ -1,32 +1,3 @@
-// import { message } from "antd";
-
-// export const beforeUpload = (file, form, fieldName) => {
-//   const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-//   if (!isJpgOrPng) {
-//     message.error("You can only upload JPG/PNG file!");
-//   }
-//   const isLt2M = file.size / 1024 / 1024 < 2;
-//   if (!isLt2M) {
-//     message.error("Image must be smaller than 2MB!");
-//   }
-//   if (isJpgOrPng && isLt2M) {
-//     form.setFieldValue(fieldName, file);
-//     console.log("uploaded!");
-//   }
-//   return false;
-// };
-
-// export const handleChange = (info) => {
-//   const {
-//     file: { status, name },
-//   } = info;
-
-//   if (status === "done") {
-//     message.success(`${name} file uploaded successfully`);
-//   } else if (status === "error") {
-//     message.error(`${name} file upload failed.`);
-//   }
-// };
 import { message } from "antd";
 
 export const beforeUpload = (helpers) => (file) => {
--- src/modules/ContactsContent/Form/FormContent.jsx
+++ src/modules/ContactsContent/Form/FormContent.jsx
@@ -12,59 +12,52 @@ export const FormContent = ({
   onSubmit,
   editMode,
   handleCancelEditModeClick,
-}) => {
-  const handleOverlayClick = (event) => {
-    event.stopPropagation();
-  };
-  return (
-    <>
-      <ModalOverlay onClick={handleOverlayClick} />
-      <ModalWrapper>
-        <StyledForm onSubmit={onSubmit}>
-          <StyledFormWrapper>
-            <LogoTitle>
-              {editMode ? "Edit contact" : "Add new contact"}
-            </LogoTitle>
+}) => (
+  <>
+    <ModalOverlay />
+    <ModalWrapper>
+      <StyledForm onSubmit={onSubmit}>
+        <StyledFormWrapper>
+          <LogoTitle>{editMode ? "Edit contact" : "Add new contact"}</LogoTitle>
 
-            <CustomPhotoInput label="Add photo" name="photo" />
+          <CustomPhotoInput label="Add photo" name="photo" />
 
-            <CustomInput
-              label="First name"
-              name="firstName"
-              type="text"
-              placeholder="First name"
-            />
-            <CustomInput
-              label="Last name"
-              name="lastName"
-              type="text"
-              placeholder="Last name"
-            />
-            <CustomInput
-              label="Email"
-              name="email"
-              type="text"
-              placeholder="Email"
-            />
-            <CustomInput
-              label="Address"
-              name="address"
-              type="text"
-              placeholder="address"
-            />
-            <Button type="submit">Save</Button>
-            <StyledBtn
-              type="button"
-              onClick={() => {
-                setShowForm(false);
-                handleCancelEditModeClick();
-              }}
-            >
-              Cancel
-            </StyledBtn>
-          </StyledFormWrapper>
-        </StyledForm>
-      </ModalWrapper>
-    </>
-  );
-};
+          <CustomInput
+            label="First name"
+            name="firstName"
+            type="text"
+            placeholder="First name"
+          />
+          <CustomInput
+            label="Last name"
+            name="lastName"
+            type="text"
+            placeholder="Last name"
+          />
+          <CustomInput
+            label="Email"
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+          <CustomInput
+            label="Address"
+            name="address"
+            type="text"
+            placeholder="address"
+          />
+          <Button type="submit">Save</Button>
+          <StyledBtn
+            type="button"
+            onClick={() => {
+              setShowForm(false);
+              handleCancelEditModeClick();
+            }}
+          >
+            Cancel
+          </StyledBtn>
+        </StyledFormWrapper>
+      </StyledForm>
+    </ModalWrapper>
+  </>
+);
