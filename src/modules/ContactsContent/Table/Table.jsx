1ee9bc5f65b3ead827ffbee4cc54d455e761c7c1
Ksenia <kseniapopova02@gmail.com>
2023-06-08 10:08:56 +0300

    move: columns into different file

--- src/modules/ContactsContent/Table/Table.jsx
+++ src/modules/ContactsContent/Table/Table.jsx
@@ -1,62 +1,9 @@
-import {
-  StyledTable,
-  StyledTableWrapper,
-  StyledLastTdWrapper,
-  StyledNameWrapper,
-  StyledAvatar,
-  StyledUserAvatar,
-} from "./style";
-import { DropDownMenu } from "../DropDownMenu";
+import { StyledTable, StyledTableWrapper } from "./style";
 import { pagination } from "./pagination";
-import { ReactComponent as Avatar } from "../../../assets/svg/avatar.svg";
+import { tableColumns } from "./columns";
 
 export const Table = ({ handleDelete, handleEdit, contacts }) => {
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-
-      render: (_, record) => (
-        <StyledNameWrapper>
-          {record.photo ? (
-            <StyledUserAvatar src={record.photo} />
-          ) : (
-            <StyledAvatar>
-              <Avatar />
-            </StyledAvatar>
-          )}
-          {record.firstName} {record.lastName}
-        </StyledNameWrapper>
-      ),
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
-    },
-    {
-      title: "Created at",
-      dataIndex: "date",
-      key: "date",
-
-      render: (_, record) => (
-        <StyledLastTdWrapper>
-          <span>{record.date}</span>
-          <DropDownMenu
-            handleDelete={handleDelete}
-            handleEdit={handleEdit}
-            contactId={record.id}
-          />
-        </StyledLastTdWrapper>
-      ),
-    },
-  ];
+  const columns = tableColumns(handleDelete, handleEdit);
 
   return (
     <StyledTableWrapper>
