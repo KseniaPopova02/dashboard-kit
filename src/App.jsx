3297e07a0b545313ebb03bb76af276cf91872ca9
Ksenia <kseniapopova02@gmail.com>
2023-06-27 17:23:54 +0300

    add: redux into App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { GlobalStyle } from "./styles/globalStyles";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {
   Forgot,
   LogIn,
@@ -19,107 +19,70 @@ import {
 } from "./pages";
 import { ROUTES } from "./routes";
 import { AuthFormLayout, MainLayout } from "./modules";
-import { useState } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
 
-const useCustomLocalStorage = (key, initialValue) => {
-  const [value, setValue] = useState(() => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
-  });
-
-  const setStoredValue = (newValue) => {
-    setValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
-  };
-  return [value, setStoredValue];
-};
-
-const App = () => {
-  const [currentUser, setCurrentUser] = useCustomLocalStorage(
-    "currentUser",
-    null
-  );
-  const [loggedIn, setLoggedIn] = useCustomLocalStorage("loggedIn", false);
-  const handleLogout = () => {
-    localStorage.removeItem("loggedIn");
-    setLoggedIn(false);
-    <Navigate to="/" />;
-  };
-
-  const updateLoggedIn = (value, user) => {
-    setLoggedIn(value);
-    setCurrentUser(user);
-  };
-
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Routes>
-        <Route
-          path={ROUTES.DASHBOARD}
-          element={
-            <MainLayout
-              currentUser={currentUser}
-              loggedIn={loggedIn}
-              handleLogout={handleLogout}
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path={ROUTES.DASHBOARD} element={<MainLayout />}>
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.OVERVIEW}`}
+              element={<View />}
             />
-          }
-        >
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.OVERVIEW}`}
-            element={<View />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.TICKETS}`}
-            element={<Tickets />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.IDEAS}`}
-            element={<Ideas />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.CONTACTS}`}
-            element={<Contacts />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.AGENTS}`}
-            element={<Agents />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.ARTICLES}`}
-            element={<Articles />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.SETTINGS}`}
-            element={<Settings />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.SUBSCRIPTION}`}
-            element={<Subscription />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.OVERVIEW_TICKETS_DETAILS}`}
-            element={<OverviewTicketsDetails />}
-          />
-          <Route
-            path={`${ROUTES.DASHBOARD}${ROUTES.OVERVIEW_TASKS}`}
-            element={<OverviewTasksPage />}
-          />
-          <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
-        </Route>
-        <Route element={<AuthFormLayout loggedIn={loggedIn} />}>
-          <Route index element={<LogIn updateLoggedIn={updateLoggedIn} />} />
-          <Route path={ROUTES.FORGOT} element={<Forgot />} />
-          <Route path={ROUTES.RESET} element={<Reset />} />
-          <Route
-            path={ROUTES.SIGNUP}
-            element={<SignUp updateLoggedIn={updateLoggedIn} />}
-          />
-          <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.TICKETS}`}
+              element={<Tickets />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.IDEAS}`}
+              element={<Ideas />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.CONTACTS}`}
+              element={<Contacts />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.AGENTS}`}
+              element={<Agents />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.ARTICLES}`}
+              element={<Articles />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.SETTINGS}`}
+              element={<Settings />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.SUBSCRIPTION}`}
+              element={<Subscription />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.OVERVIEW_TICKETS_DETAILS}`}
+              element={<OverviewTicketsDetails />}
+            />
+            <Route
+              path={`${ROUTES.DASHBOARD}${ROUTES.OVERVIEW_TASKS}`}
+              element={<OverviewTasksPage />}
+            />
+            <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+          </Route>
+          <Route element={<AuthFormLayout />}>
+            <Route index element={<LogIn />} />
+            <Route path={ROUTES.FORGOT} element={<Forgot />} />
+            <Route path={ROUTES.RESET} element={<Reset />} />
+            <Route path={ROUTES.SIGNUP} element={<SignUp />} />
+            <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
